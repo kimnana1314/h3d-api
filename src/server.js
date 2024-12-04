65488e80991da69506ceb70f15142a95fe61e712
Hair Tran Van <haih14@gmail.com>
2024-12-04 10:51:55 +0700

    first commit

---
+++ src/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app =express()
+const hostname='localhost'
+const port =3000
+
+
+app.get('/', function (req, res) {
+  res.send('<h1>Hello World</h1>')
+})
+
+app.listen(port,hostname,()=>{
+  console.log('server run port:',port)
+})
+
+
+
